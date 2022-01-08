f37235a0e519fbf7a50bb2854e8e8762167ca828
gadekarmd <gadekarmd929293@gmail.com>
2022-01-09 00:12:42 +0530

    db

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,7 +4,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    uri = os.getenv("DATABASE_URL")  # or other relevant config var
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     MAIL_SERVER = 'smtp.googlemail.com'
