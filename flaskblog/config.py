1f2ca3120370d544ff58f3757144f7a3004af5ae
gadekarmd <gadekarmd929293@gmail.com>
2022-01-09 08:08:57 +0530

    secret key

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,8 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.urandom(32)
+    #SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
